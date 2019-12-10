cc8d5bd9211433179a0385c531d86eb02b6c846d
徐俊杰 <yinhunfeixue@163.com>
2019-12-10 14:32:44 +0800

    feat: 清理多余的文件

--- src/base/interfaces/IComponentProps copy.ts
+++
@@ -1,8 +0,0 @@
-import { MouseEventHandler } from 'react';
-
-export default interface IComponentProps {
-  className?: string;
-  style?: { [key: string]: any };
-  data?: any;
-  onClick?: MouseEventHandler;
-}
