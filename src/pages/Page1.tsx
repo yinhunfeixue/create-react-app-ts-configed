8d4b5514903ef67c03e572cc848477338224c20e
徐俊杰 <yinhunfeixue@163.com>
2023-06-16 19:43:52 +0800

    feat: 滚动条组件

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,9 +1,10 @@
 import IComponentProps from '@/base/interfaces/IComponentProps';
 import ScrollBar from '@/pages/component/ScrollBar';
 import ScrollBarDirection from '@/pages/component/ScrollBarDirection';
+import ScrollBarUtil from '@/pages/component/ScrollBarUtil';
 import { Button } from 'antd';
 import Axios from 'axios';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 interface IPage1State {
   visibleDrawer: boolean;
@@ -26,7 +27,15 @@ class Page1 extends Component<IPage1Props, IPage1State> {
   }
   componentDidMount() {
     Axios.get('baiduApi?wd=create-react-app');
+
+    if (this.divRef.current) {
+      ScrollBarUtil.listenScrollChange(this.divRef.current, (data) => {
+        console.log('data', data);
+      });
+    }
   }
+
+  private divRef: React.RefObject<HTMLDivElement> = React.createRef();
 
   render() {
     const { maxValue, value } = this.state;
@@ -44,6 +53,23 @@ class Page1 extends Component<IPage1Props, IPage1State> {
           direction={ScrollBarDirection.H}
           onChange={(value) => this.setState({ value })}
         />
+
+        <Button
+          onClick={() => {
+            if (this.divRef.current) {
+              const { scrollWidth, scrollHeight, offsetWidth, offsetHeight } =
+                this.divRef.current;
+              console.log(scrollWidth, scrollHeight, offsetWidth, offsetHeight);
+            }
+          }}
+        >
+          显示尺寸
+        </Button>
+        <div ref={this.divRef} style={{ width: 300, border: '1px solid #888' }}>
+          <div style={{ width: 500, border: '1px solid red' }} contentEditable>
+            content
+          </div>
+        </div>
       </div>
     );
   }
