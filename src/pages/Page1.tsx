a88c618af3a18580faa1435d310f9551570e9b2c
徐俊杰 <yinhunfeixue@163.com>
2024-01-31 14:25:06 +0800

    更新 ide 配置

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,6 +1,4 @@
 import IComponentProps from '@/base/interfaces/IComponentProps';
-import UrlUtil from '@/utils/UrlUtil';
-import { Button, Drawer } from 'antd';
 import Axios from 'axios';
 import { Component } from 'react';
 
@@ -24,34 +22,7 @@ class Page1 extends Component<IPage1Props, IPage1State> {
   }
 
   render() {
-    const { visibleDrawer } = this.state;
-    return (
-      <div>
-        Page1
-        <Button
-          onClick={() => {
-            UrlUtil.toUrl(`/Page2/page21`, { x: 1 });
-          }}
-        >
-          打开页面二一
-        </Button>
-        <Button onClick={() => this.setState({ visibleDrawer: true })}>
-          打开drawer
-        </Button>
-        {visibleDrawer && (
-          <Drawer
-            title="drawer"
-            open={visibleDrawer}
-            closable
-            onClose={() => {
-              this.setState({ visibleDrawer: false });
-            }}
-          >
-            aa
-          </Drawer>
-        )}
-      </div>
-    );
+    return <div style={{ whiteSpace: 'pre' }}>aaa\r\nbbbb{'\r\n'}ccc</div>;
   }
 }
 
