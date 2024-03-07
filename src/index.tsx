1c51b0e9e02f6af7f066575e7f5945d531eaa778
徐俊杰 <yinhunfeixue@163.com>
2024-03-07 18:17:35 +0800

    添加常用样式

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import '@/fonts/iconfont.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.less';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
