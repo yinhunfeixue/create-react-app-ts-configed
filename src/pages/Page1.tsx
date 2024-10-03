62304ea7aac46d71d19c2f1cd255ec59d780e97b
徐俊杰 <xujunjie1@xiaomi.com>
2024-10-03 22:31:14 +0800

    sql

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,8 +1,8 @@
+import { Input } from 'antd';
 import classNames from 'classnames';
-import { CSSProperties } from 'react';
+import { MySQL } from 'dt-sql-parser';
+import { CSSProperties, useState } from 'react';
 
-const img = require('../assets/a.png');
-console.log(img);
 interface IPage1Props {
   className?: string;
   style?: CSSProperties;
@@ -12,9 +12,41 @@ interface IPage1Props {
  */
 function Page1(props: IPage1Props) {
   const { className, style } = props;
+
+  const [inputValue, setInputValue] = useState<string>(
+    'select x as x123 from aaa'
+  );
+  const [sqlError, setSqlError] = useState<any[]>();
+
+  console.log('sqlError', sqlError);
+
+  const updateInputValue = (value: string) => {
+    setInputValue(value);
+    updateSqlError(value);
+  };
+
+  const updateSqlError = (sql: string) => {
+    const error: any[] = [];
+    if (inputValue) {
+      try {
+        const parser = new MySQL();
+        const ast = parser.parse(inputValue);
+        console.log('ast', ast);
+      } catch (error) {
+        console.log('error', error);
+      }
+    }
+    setSqlError(error);
+    return error;
+  };
   return (
     <div className={classNames(className)} style={style}>
-      Page1
+      <Input.TextArea
+        value={inputValue}
+        onChange={(event) => {
+          updateInputValue(event.target.value);
+        }}
+      />
     </div>
   );
 }
